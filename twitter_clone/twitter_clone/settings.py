542eb4756ed85b1c450548d19e4b6d68605354d9
yajya <yajyaarora18@gmail.com>
2026-05-05 14:17:13 +0530

    database

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'twitter_clone.wsgi.application'
 # fall back to SQLite for local development.
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
