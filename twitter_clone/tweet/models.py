9afcf51a0965615002373c3a4004b52db792df90
yajya <yajyaarora18@gmail.com>
2026-05-05 11:55:04 +0530

    final

--- twitter_clone/tweet/models.py
+++ twitter_clone/tweet/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Tweet(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     text=models.TextField(max_length=240)
-    photo=models.ImageField(upload_to='photos/', blank=True, null=True)
+    photo=models.ImageField(upload_to='photos/', blank=True, null=True, default='photos/download.jpg')
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
